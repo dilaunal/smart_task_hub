9690130013b6e75d1ef8f0997a1a16e56bf82863
dilaunal <dila.unal00@gmail.com>
2026-03-13 17:35:37 +0300

    İlk sürüm: Backend ve Frontend hazır

---
+++ backend/main.py
@@ -0,0 +1,59 @@
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+import models
+import database
+from fastapi import HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+
+models.Base.metadata.create_all(bind=database.engine)
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"], 
+    allow_headers=["*"], 
+)
+
+def get_db():
+    db = database.SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.get("/tasks")
+def get_tasks(db: Session = Depends(get_db)):
+    return db.query(models.TaskModel).all()
+
+
+@app.post("/tasks")
+def create_task(title: str, description: str, db: Session = Depends(get_db)):
+    new_task = models.TaskModel(title=title, description=description)
+    db.add(new_task)
+    db.commit()
+    db.refresh(new_task)
+    return new_task
+
+@app.put("/tasks/{task_id}")
+def update_task(task_id: int, completed: bool, db: Session = Depends(get_db)):
+    task = db.query(models.TaskModel).filter(models.TaskModel.id == task_id).first()
+    if not task:
+        raise HTTPException(status_code=404, detail="Görev bulunamadı")
+    
+    task.completed = completed
+    db.commit()
+    db.refresh(task)
+    return task
+
+@app.delete("/tasks/{task_id}")
+def delete_task(task_id: int, db: Session = Depends(get_db)):
+    task = db.query(models.TaskModel).filter(models.TaskModel.id == task_id).first()
+    if not task:
+        raise HTTPException(status_code=404, detail="Görev bulunamadı")
+    
+    db.delete(task)
+    db.commit()
+    return {"message": f"ID'si {task_id} olan görev başarıyla silindi"}
