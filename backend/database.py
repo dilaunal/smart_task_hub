067d2e6be40d78ab2587625c15fdfac6e011f102
dilaunal <dila.unal00@gmail.com>
2026-03-26 18:47:41 +0300

    Final check for tasks endpoint

--- backend/database.py
+++ backend/database.py
@@ -2,6 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+from backend import models
+
 SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
@@ -9,3 +11,4 @@ engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+models.Base.metadata.create_all(bind=engine)
